08b98bd9ffadc39e0c57926770dd3ea642e1c0e8
RafaelKr <coding@rafael.kr>
2022-04-26 16:06:00 +0200

    wip

--- prisma/alter-extensions.sh
+++ prisma/alter-extensions.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-source /scripts/env-data.sh
-
 if [ -z "${ALTER_EXTENSIONS}" ]; then
 	ALTER_EXTENSIONS=$POSTGRES_MULTIPLE_EXTENSIONS
+fi
+
+if [ -z "${ALTER_EXTENSION_USER}" ]; then
+  # first db
+	ALTER_EXTENSION_USER=$POSTGRES_USER
 fi
 
 if [ -z "${ALTER_EXTENSION_DB}" ]; then
@@ -22,12 +25,13 @@ if [ -z "${ALTER_EXTENSION_SCHEMA}" ]; then
 fi
 
 echo "ALTER_EXTENSIONS: $ALTER_EXTENSIONS";
+echo "ALTER_EXTENSION_USER: $ALTER_EXTENSION_USER";
 echo "ALTER_EXTENSION_DB: $ALTER_EXTENSION_DB";
 echo "ALTER_EXTENSION_SCHEMA: $ALTER_EXTENSION_SCHEMA";
 
 for ext in $(echo ${ALTER_EXTENSIONS} | tr ',' ' '); do
     if [[ ${ext} != 'pg_cron' ]]; then
-      su - postgres -c "psql -c \"UPDATE pg_extension SET extrelocatable = true WHERE extname = '${ext}'; ALTER EXTENSION $ext SET SCHEMA $ALTER_EXTENSION_SCHEMA;\" $ALTER_EXTENSION_DB"
+      psql ${ALTER_EXTENSION_DB} -U ${ALTER_EXTENSION_USER} -p 5432 -h localhost -c "UPDATE pg_extension SET extrelocatable = true WHERE extname = '${ext}'; ALTER EXTENSION $ext SET SCHEMA $ALTER_EXTENSION_SCHEMA;"
     fi
 done
 
